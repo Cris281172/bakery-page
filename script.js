d8c5abbdb65a2d998af32a221cf85d7cd919287c
Cris281172 <krzysiu.juczynski@gmail.com>
2021-05-31 22:23:06 +0200

    (Add slides sections)

--- script.js
+++ script.js
@@ -26,6 +26,67 @@ scroll = e => {
 
 }
 
+window.onscroll = () => slides();
+
+console.log(window.height )
+
+const slides = e => {
+
+    if(document.body.scrollTop > 150 || document.documentElement.scrollTop > 150){
+        document.querySelector(`.welcome_article`).style.animationName = `slideUp`;
+        document.querySelector(`.welcome_article`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 650 || document.documentElement.scrollTop > 650){
+        document.querySelector(`.cooking_article`).style.animationName = `slideRight`;
+        document.querySelector(`.cooking_article`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500){
+        document.querySelector(`.our_baking`).style.animationName = `slideOpacity`;
+        document.querySelector(`.our_baking`).style.opacity = `1`
+    }
+    if(document.body.scrollTop > 2200 || document.documentElement.scrollTop > 2200){
+        document.querySelector(`.lemon_cake_article`).style.animationName = `slideUp`;
+        document.querySelector(`.lemon_cake_article`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700){
+        document.querySelector(`.service`).style.animationName = `slideRight`;
+        document.querySelector(`.service`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 3100 || document.documentElement.scrollTop > 3100){
+        document.querySelector(`.gallery`).style.animationName = `slideOpacity`;
+        document.querySelector(`.gallery`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 3900 || document.documentElement.scrollTop > 3900){
+        document.querySelector(`.our_blog`).style.animationName = `slideUp`;
+        document.querySelector(`.our_blog`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 4600 || document.documentElement.scrollTop > 4600){
+        document.querySelector(`.contact_us`).style.animationName = `slideRight`;
+        document.querySelector(`.contact_us`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 5400 || document.documentElement.scrollTop > 5400){
+        document.querySelector(`.contact_us_details`).style.animationName = `slideOpacity`;
+        document.querySelector(`.contact_us_details`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 6200 || document.documentElement.scrollTop > 6200){
+        document.querySelector(`.find_us`).style.animationName = `slideUp`;
+        document.querySelector(`.find_us`).style.opacity = `1`
+    }
+
+    if(document.body.scrollTop > 6800 || document.documentElement.scrollTop > 6800){
+        document.querySelector(`.main_footer`).style.animationName = `slideRight`;
+        document.querySelector(`.main_footer`).style.opacity = `1`
+    }
+}
+
 menuElement.forEach(el => el.addEventListener(`click`,scroll));
 
 
