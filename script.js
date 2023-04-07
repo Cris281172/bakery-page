206b3c11d45d541174cac059890181f68f0f4dbb
Krzysztof Juczyński <72098823+Cris281172@users.noreply.github.com>
2023-04-07 09:04:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const arrow = document.querySelector(`.arrow`);
 const menuElement = document.querySelectorAll(`.main_header`);
-
+const hamburgerMenu = document.querySelector('.hamburger_menu')
+const menu = document.querySelector('.menu')
+const arrowUp = document.querySelector('.arrow_up')
 const scroll = e => {
     e.preventDefault();
     document.querySelector(e.target.getAttribute('href')).scrollIntoView({behavior: "smooth"})
 }
 
-const detectElementInViewport = el => {
-    const rect = el.getBoundingClientRect();
-    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+const activeMobileMenu = () => {
+    hamburgerMenu.classList.toggle('active');
+    menu.classList.toggle('active');
 }
 
+const visibleArrowUp = () => {
+    if(window.pageYOffset  > 250){
+        arrowUp.classList.add('active');
+    }
+    else{
+        arrowUp.classList.remove('active');
+    }
+}
 
-let timeout;
-
-window.addEventListener('scroll', () => {
-
-
-    clearTimeout(timeout);
-    timeout = setTimeout(slides, 100);
-    slides()
-});
-
+const scrollToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+visibleArrowUp()
 menuElement.forEach(el => el.addEventListener(`click`,scroll));
-
-
+hamburgerMenu.addEventListener('click', activeMobileMenu)
+document.addEventListener('scroll', visibleArrowUp)
+arrowUp.addEventListener('click', scrollToTop)
